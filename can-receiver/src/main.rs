6b1b2a7b24f8d9bc98dd7652f0cd2cf9034b1841
Aleksander Jóźwik <aleksanderjx@gmail.com>
2025-10-28 00:11:52 +0100

    Add a proper dbc decoding

--- can-receiver/src/main.rs
+++ can-receiver/src/main.rs
@@ -1,26 +1,12 @@
-mod config;
+mod setup;
+mod integration;
 
 use color_eyre::eyre::Result;
-use embedded_can::{blocking::Can, Frame as FrameTrait, Id};
-use waveshare_usb_can_a::{self as ws, Frame};
-use std::fs;
-use can_dbc::{Dbc, MessageId};
+use embedded_can::blocking::Can;
+use waveshare_usb_can_a::{self as ws};
+use setup::config;
 
-fn id_to_message_id(id: &Id) -> MessageId {
-        match id {
-        Id::Standard(sid) => MessageId::Standard(sid.as_raw()),
-        Id::Extended(eid) => MessageId::Extended(eid.as_raw()),
-    }
-}
-
-fn connect_bytes(data: &[u8]) -> Option<f64> {
-    match data.len() {
-        1 => Some(data[0] as f64),
-        2 => Some((((data[0] as u16) << 8) | (data[1] as u16)) as f64),
-        3 => Some((((data[0] as u32) << 16) | ((data[1] as u32) << 8) | (data[2] as u32)) as f64),
-        _ => None,
-    }
-}
+use crate::integration::dbc_handler::DbcHandler;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -35,71 +21,42 @@ fn main() -> Result<()> {
     println!("Using device: {}", device_port);
     println!("CAN speed: {:?}", can_baud_rate);
 
-    // can dbc tests
+    // Initialize DBC decoding
+    let dbc = DbcHandler::new()?;
 
-    let data = fs::read_to_string(config::find_first_dbc_in_exe_dir()?.unwrap()).expect("Unable to read input file");
-    let dbc = Dbc::try_from(data.as_str()).expect("Failed to parse dbc file");
-
-    let frame1 = Frame::new(Id::Standard(embedded_can::StandardId::new(129).unwrap()), &[11, 223]).unwrap();
-    let frame2 = Frame::new(Id::Standard(embedded_can::StandardId::new(130).unwrap()), &[49, 231]).unwrap();
-
-    let message_id = id_to_message_id(&frame2.id());
-    
-    // Znajdź wiadomość w DBC
-    if let Some(message) = dbc.messages().iter().find(|m| *m.id() == message_id) {
-        println!("\nMessage: {} (ID: {:?}, Size: {} bytes)", message.name(), message.id(), message.size());
-        
-        let signal = message.signals().iter().next().unwrap();
-        println!("  Signal: {}", signal.name());
-        println!("    Start bit: {}", signal.start_bit);
-        println!("    Size: {} bits", signal.size);
-        println!("    Factor: {}", signal.factor);
-        println!("    Offset: {}", signal.offset);
-        println!("    Min: {}", signal.min);
-        println!("    Max: {}", signal.max);
-        println!("    Unit: {}", signal.unit());
-        println!("    Byte order: {:?}", signal.byte_order());
-        println!("    Value type: {:?}", signal.value_type());
-
-        let data = connect_bytes(frame2.data()).unwrap();
-        let result = data * signal.factor - signal.offset;
-
-        println!("{} {}", result, signal.unit());
-
-    } else {
-        println!("\nMessage with ID {:?} not found in DBC", message_id);
-    }
-
-    // end of tests
+    println!("DBC loaded: {} message definitions available", dbc.dbc.messages().len());
 
     // CAN configuration
     let ws_config = ws::Usb2CanConfiguration::new(can_baud_rate)
         .set_loopback(false)
         .set_silent(false);  // Enable receiving frames from the bus
 
-    // Initialize connection with receive timeout
+    // Initialize connection
     let mut device = ws::sync::new(&device_port, &ws_config)
+        // we won't use timeout as .receive() is blocking
         // .set_serial_receive_timeout(Duration::from_millis(10000))
         .open()?;
 
     println!("Starting to receive CAN frames... (Press Ctrl+C to stop)");
 
+    let mut name;
+    let mut value;
+    let mut unit;
+
     loop {
         match device.receive() {
             Ok(frame) => {
-                println!("Frame: ID={:?}, Data={:?}", frame.id(), frame.data());
-                // ID dostajemy w decimalu i w DBC jest tak samo (a na CANie w hex, ale nevemind)
+                (name, value, unit) = dbc.decode(frame);
+                println!("{}: {} {}", name, value, unit);
             }
             
             Err(ws::sync::Error::SerialReadTimedOut) => {
-                // Timeout - normal case, continue listening
                 println!("Timeout - no frame received, continuing...");
                 continue;
             }
             
             Err(e) => {
                 eprintln!("Error receiving frame: {}", e);
-                // thread::sleep(Duration::from_millis(100));
             }
         }
     }
